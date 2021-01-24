cba02d26dc755e83dfe6eb97bd18bdd666102f66
Shuvam <shuvamwap4@gmail.com>
2021-01-24 21:09:09 +0530

    Bitcoin

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+const about =() => {
+
+    return(
+        <Layout>
+    <div>
+   
+     
+    
+    <h1>About BitPrice</h1>
+    <p>Application to view Bitcoin prices</p>
+    </div>
+    </Layout>
+    )
+}
+
+export default about
